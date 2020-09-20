db50e7609179620c996c572a12951152dd561ce0
rosid1878 <71593956+rosid1878@users.noreply.github.com>
2020-09-20 19:40:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://discord-tutoril.glitch.me"]
+const urls = ["https://kosan-apart.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
